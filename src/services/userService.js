e091bb764a3c017a6ea77e4a9339c2321a2b0325
Raouia <raouiaabourachid@gmail.com>
2025-10-22 16:04:03 +0100

    feat(auth): implement JWT authentifcation and secure API routes

--- src/services/userService.js
+++ src/services/userService.js
@@ -10,7 +10,7 @@ export const createUser = async (username, email, password) => {
 
 
 export const findUserByUsername = async (username) => {
-    return prisma.User.findUnique({
+    return prisma.user.findUnique({
         where: { username },
     });
 };
@@ -20,3 +20,16 @@ export const findUserByEmail = async (email) => {
         where: { email },
     });
 }
+
+export const updateUserScoreById = async (id, score) => {
+  try {
+    return await prisma.user.update({
+      where: { id },
+      data: { score },
+      select: { id: true, username: true, email: true, score: true }
+    });
+  } catch (e) {
+    if (e.code === "P2025") return null; // record not found
+    throw e;
+  }
+};
